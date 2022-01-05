d365560117b2069472371d6eba4df59b97f574c5
Isemman James Arrojo <70742691+imb4n@users.noreply.github.com>
2022-01-05 15:26:39 +0800

    Create Caesars Cipher.js

---
+++ Caesars Cipher.js
@@ -0,0 +1,6 @@
+function rot13(str) {
+  let alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+
+  return str.replace(/[A-Z]/gi, (match) => alpha[alpha.indexOf(match) + 13])
+  
+}
